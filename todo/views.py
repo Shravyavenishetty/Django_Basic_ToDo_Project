b1b9762408ee387bc2a957d2f9e63c2993ffaf24
Venishetty Shravya <shravyavenishetty03@gmail.com>
2025-07-20 14:26:47 +0530

    added post request

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from .models import Task
+from django.shortcuts import redirect
 
-# Create your views here.
+def addTask(request):
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
